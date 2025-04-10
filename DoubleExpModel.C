e543a63b0e516751d7855b2a02c97da9caafd3a6
szymeeeek <sz.machura@student.uj.edu.pl>
2025-04-10 16:53:07 +0200

    ...

--- DoubleExpModel.C
+++ DoubleExpModel.C
@@ -23,8 +23,16 @@ Double_t deModel(std::string filename = ""){
         return -1;
     }
 
+    //opening the output file
+    TFile *output = new TFile(filename.c_str(), "READ");
+    if(!spectra->IsOpen()){
+        std::cerr << "File not opened!" << std::endl;
+        return -1;
+    }
+
     //creating the graph and the function for fitting
-    TGraph *DE = new TGraph();
+    TGraphErrors *DE = new TGraphErrors();
+
     TF1 *deF = new TF1("deF", deFunc, 0, 1000, 4);
     deF->SetParNames("I1", "lambda1", "I2", "lambda2");
     deF->SetParameters(1, 1, 1, 1);
@@ -40,7 +48,10 @@ Double_t deModel(std::string filename = ""){
 
     //fitting the graph
     TFitResultPtr fit = DE->Fit(deF, "S");
-        //dodać tekst na wykresach
+
+    std::string paramsString = Form("#lambda_{1} = %f +/- %f\n I_{1} = %f +/- %f\n #lambda_{2} = %f +/- %f\n I_{2} = %f +/- %f", deF->GetParameter(0), deF->GetParError(0), 
+                                            deF->GetParameter(1), deF->GetParError(1), deF->GetParameter(2), deF->GetParError(2), deF->GetParameter(3), deF->GetParError(3));
+    TLatex *paramsText = new TLatex();
 
     //creating the canvas for the graph
     TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
@@ -50,6 +61,8 @@ Double_t deModel(std::string filename = ""){
     DE->SetMarkerColor(kBlue);
     DE->SetLineColor(kBlue);
     DE->SetLineWidth(2);
+    DE->Draw("AP")
+    paramsText->DrawLatex(.8, .8, paramsString);
 
     return 0;
 }
