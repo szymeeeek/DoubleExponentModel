c8d9d41e41940c9d96bbc7603ee833513d112f18
Szymon <sz.machura@student.uj.edu.pl>
2025-04-29 14:11:10 +0200

    Citiroc data analysis facilitated

--- DoubleExpModel.C
+++ DoubleExpModel.C
@@ -15,31 +15,48 @@ p[2] is the constant term for \lambda_2
 p[3] -> \lambda_2
 */
 
-Double_t deModel(std::string filename = "250410"){
+Double_t deModel(std::string directory = "/home/szymon/LHCb/20250428firstMeasCitiroc/BCF20XL2/"){
     //opening the file with fitted spectra
-    // TFile *spectra = new TFile(filename.c_str(), "READ");
-    // if(!spectra->IsOpen()){
-    //     std::cerr << "File not opened!" << std::endl;
-    //     return -1;
-    // }
+    std::string specFile = Form("%soutput_HISTOS.root", directory.c_str());
+    TFile *spectra = new TFile(specFile.c_str(), "READ");
+    if(!spectra->IsOpen()){
+        std::cerr << "File not opened!" << std::endl;
+        return -1;
+    }
 
     //opening the output file
-    TFile *output = new TFile(Form("%s.root", filename.c_str()), "RECREATE");
-    // if(!spectra->IsOpen()){
-    //     std::cerr << "File not opened!" << std::endl;
-    //     return -1;
-    // }
+    TFile *output = new TFile("output_DEmodel.root", "RECREATE");
+    if(!output->IsOpen()){
+        std::cerr << "File not opened!" << std::endl;
+        return -1;
+    }
+
+    std::vector <Float_t> mean, erMean, pos, erPos;
+    for(Int_t i = 1; i<41; i++){
+        std::string hName = Form("Run%i_PHA_LG_0_50.txt", i);
+
+        TH1I *hist = (TH1I*)spectra->Get(hName.c_str());
+        std::string fName = Form("%s_fit", hName.c_str());
+        TF1 *func = hist->GetFunction(fName.c_str());
+
+        mean.push_back(func->GetParameter(1));
+        erMean.push_back(func->GetParError(1));
+        pos.push_back(i*10);
+        erPos.push_back(0.1);
+    }
+    Int_t n = pos.size();
 
     //creating the graph and the function for fitting
-    TGraphErrors *DE = new TGraphErrors(Form("%s.txt", filename.c_str()), "%lg %lg %lg %lg");
+    //TGraphErrors *DE = new TGraphErrors(Form("%s.txt", filename.c_str()), "%lg %lg %lg %lg");
+    TGraphErrors *DE = new TGraphErrors(n, pos.data(), mean.data(), erPos.data(), erMean.data());
 
-    TF1 *deF = new TF1("deF", deFunc, 0, 300, 4);
+    TF1 *deF = new TF1("deF", deFunc, 50, 410, 4);
     deF->SetParNames("I1", "lambda1", "I2", "lambda2");
-    deF->SetParameters(3, 450, 3, 40);
+    deF->SetParameters(3, 4500, 3, 40);
     deF->SetParLimits(0, 0, 1e2);
-    deF->SetParLimits(1, 0, 1e3);
+    //deF->SetParLimits(1, 0, 6e3);
     deF->SetParLimits(2, 0, 1e2);
-    deF->SetParLimits(3, 0, 400);
+    deF->SetParLimits(3, 0, 40);
    
     //reading data from the file with fitted spectra
     // for(){
@@ -55,14 +72,14 @@ Double_t deModel(std::string filename = "250410"){
 
     //creating the canvas for the graph
     TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
-    DE->SetTitle("DE Model; position [cm]; Q [C]");
+    DE->SetTitle("DE Model; position [cm]; Q [a.u.]");
     DE->SetMarkerStyle(20);
     DE->SetMarkerSize(0.5);
     DE->SetMarkerColor(kBlue);
     DE->SetLineColor(kBlue);
     DE->SetLineWidth(2);
     DE->Draw("AP");
-    paramsText->DrawLatex(.8, .8, paramsString.c_str());
+    paramsText->DrawLatex(300, 35, paramsString.c_str());
 
     c1->Write();
     DE->Write();
